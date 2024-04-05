c8627490c06e2ff585e2f767c35a82636650eac2
gquence <ma.anshukov@gmail.com>
2024-04-05 21:08:12 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <queue>
+#include <string>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        return 1;
+    }
+    auto queueDefaultMaxSize = std::stoi(av[1]);
+    std::queue<std::string> Q;
+    int nestingLevel = 0;
+    auto printAndPopAllCmds = [&Q] () {
+        if (Q.size()) {
+            std::cout << "bulk: ";
+            while ( Q.size() ) {
+                std::cout << Q.front();
+                Q.pop();
+                if (Q.size())  {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << std::endl;
+        }
+    };
+    for (std::string newLine; std::getline(std::cin, newLine);) {
+        if (newLine == "{") {
+            if (nestingLevel == 0) {
+                printAndPopAllCmds();
+            }
+            nestingLevel++;
+            continue;
+        }
+        if (newLine == "}") {
+            nestingLevel--;
+            if (nestingLevel == 0) {
+                printAndPopAllCmds();    
+            }
+            continue;
+        }
+        Q.push(newLine);
+        if (nestingLevel == 0  && Q.size() == queueDefaultMaxSize) {
+            printAndPopAllCmds();
+        }
+    }
+    return 0;
+}
